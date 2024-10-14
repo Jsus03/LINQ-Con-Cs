23b0239b36668e78cd862c7497463a57311924c7
Practicantes <platinointerns@gmail.com>
2024-10-14 16:57:06 -0600

    commit

---
+++ Producto.cs
@@ -0,0 +1,10 @@
+namespace LINQ_Con_Cs;
+
+public class Producto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public DateTime FecheDeAlta { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿static IEnumerable<string> Personas()
+{
+    yield return "Francisco";
+    yield return "Misael";
+    yield return "Jesus";
+    yield return "Pedro";
+    yield return "Pablo";
+    yield return "Javier";
+}
+
+static IEnumerable<string> Apellidos()
+{
+    yield return "Calderon";
+    yield return "Hernandez";
+    yield return "Fernandez";
+    yield return "Lopez";
+    yield return "Cazerez";
+}
+
+List<Producto> productos = new List<Producto>()
+{
+    new Producto() 
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now},
+    {Id = 1, Nombre = "Leche", Descripcion = "Leche Entera", Precio = 25.0m, FechaDeAlta = DateTime.Now}
+}
+
+System.Console.WriteLine("Query Syntax");
+var personas1 = (from p in Personas()
+                from a in Apellidos()
+                select new {
+                    Nombre = p, 
+                    Apellido = a
+                });
+
+foreach (var persona in personas1)
+{
+    System.Console.WriteLine($"Nombre: {persona.Nombre} Apellido: {persona.Apellido}");
+} 
+
+System.Console.WriteLine("\nSyntax Method");
+var personas = Personas().SelectMany(persona => Apellidos()
+                    .Select(apellido => new {
+                    Nombre = persona,
+                    Apellido = apellido})
+                );
+
+foreach (var persona in personas)
+{
+    System.Console.WriteLine($"Nombre:{persona.Nombre} Apellido:{persona.Apellido}");
+}
