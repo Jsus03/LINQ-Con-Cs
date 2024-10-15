5236dad8940f282ce0cadb9c350572cef0e978c4
JesusDanC <jesusdanielcg3@gmail.com>
2024-10-14 21:46:33 -0600

    Se terminaron las seccion 1, 2 y 3 del curso.

--- obj/Debug/net8.0/LINQ-Con-Cs.AssemblyInfo.cs
+++ obj/Debug/net8.0/LINQ-Con-Cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ-Con-Cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc5f5d1099b8e651054253563f6042502a7dbc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b0239b36668e78cd862c7497463a57311924c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ-Con-Cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ-Con-Cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
